2be50e60fd9756440ab0912c9287078f4788bc60
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-27 18:51:53 -0300

    refactor: change event type for itens

--- stages/Plains.cs
+++ stages/Plains.cs
@@ -23,11 +23,11 @@ namespace reign_of_grelok_wpf.stages
 
             menu = new List<StageMenuItem>();
             menu.Add(new StageMenuItem("Olhar ao redor", _ => this.ShowStageMessage(), EventType.Text, true));
-            menu.Add(new StageMenuItem("Ir para Norte", _ => this.ShowStageMessage(), EventType.Load, true));
-            menu.Add(new StageMenuItem("Ir para Sul", _ => this.ShowStageMessage(), EventType.Load, true));
-            menu.Add(new StageMenuItem("Ir para Leste", _ => this.ShowStageMessage(), EventType.Load, true));
-            menu.Add(new StageMenuItem("Ir para Oeste", _ => this.ShowStageMessage(), EventType.Load, true));
-            menu.Add(new StageMenuItem("Inventário", _ => this.ShowStageMessage(), EventType.Load, true));
+            menu.Add(new StageMenuItem("Ir para Norte", _ => this.ShowStageMessage(), EventType.Text, true));
+            menu.Add(new StageMenuItem("Ir para Sul", _ => this.ShowStageMessage(), EventType.Text, true));
+            menu.Add(new StageMenuItem("Ir para Leste", _ => this.ShowStageMessage(), EventType.Text, true));
+            menu.Add(new StageMenuItem("Ir para Oeste", _ => this.ShowStageMessage(), EventType.Text, true));
+            menu.Add(new StageMenuItem("Inventário", this.inventoryInstance.LoadStageInfo, EventType.Load, true));
         }
 
         public StageInfo LoadStageInfo()
