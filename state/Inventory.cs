1baab10a260e7adcdb9634ccff3a1242253f67d6
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-27 18:52:16 -0300

    fix: item key

--- state/Inventory.cs
+++ state/Inventory.cs
@@ -33,7 +33,7 @@ namespace reign_of_grelok_wpf.state
 
             this.menu = new List<StageMenuItem>();
             menu.Add(new StageMenuItem("Espada enferrujada", _ => this.showItemDescription("Espada enferrujada"), EventType.Text, true));
-            menu.Add(new StageMenuItem("Frasco de bebida", _ => this.showItemDescription("Espada enferrujada"), EventType.Text, true));
+            menu.Add(new StageMenuItem("Frasco de bebida", _ => this.showItemDescription("Frasco de bebida"), EventType.Text, true));
 
             itens = new Dictionary<string, InventoryItem>();
             itens.Add("Espada enferrujada", new InventoryItem("Espada enferrujada", "Sua arma. Enferrujada, mas confiável.", true));
@@ -74,102 +74,13 @@ namespace reign_of_grelok_wpf.state
             return item.description;
         }
 
-        public void Load(CallbackStageMenu callback)
-        {
-            Console.WriteLine("REINO DE GRELOK (beta v.632)");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("\nInventário");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("Pressione de acordo com o item:");
-            if (rustSword) Console.WriteLine("1 - Espada enferrujada");
-            if (drinkingFlask) Console.WriteLine("2 - Frasco de bebida");
-            if (zombieHead) Console.WriteLine("3 - Cabeça de zumbi");
-            if (refinedGemStone) Console.WriteLine("4 - Pedra preciosa refinada");
-            if (magicalShard) Console.WriteLine("5 - Fragmento mágico");
-            if (magicSword) Console.WriteLine("6 - Espada mágica");
-            if (brassKey) Console.WriteLine("7 - Chave de latão");
-            if (rawGemStone) Console.WriteLine("8 - Pedra preciosa bruta");
-            if (fullFlask) Console.WriteLine("9 - Frasco de bebida cheio.");
-            Console.WriteLine("B - Voltar");
-            Console.WriteLine("Q - Sair");
-            var keyInfo = Console.ReadKey();
-            var key = keyInfo.KeyChar;
-
-            //this.LoadOptions(key, callback);
-        }
-
-        /*private void LoadOptions(char key, CallbackStageMenu callback)
-        {
-            switch (key)
-            {
-                case '1':
-                    this.checkIfItemISAvailable(rustSword, callback);
-                    this.showItemDescription(0);
-                    this.Load(callback);
-                    break;
-                case '2':
-                    this.checkIfItemISAvailable(drinkingFlask, callback);
-                    this.showItemDescription(1);
-                    this.Load(callback);
-                    break;
-                case '3':
-                    this.checkIfItemISAvailable(zombieHead, callback);
-                    this.showItemDescription(2);
-                    this.Load(callback);
-                    break;
-                case '4':
-                    this.checkIfItemISAvailable(refinedGemStone, callback);
-                    this.showItemDescription(3);
-                    this.Load(callback);
-                    break;
-                case '5':
-                    this.checkIfItemISAvailable(magicalShard, callback);
-                    this.showItemDescription(4);
-                    this.Load(callback);
-                    break;
-                case '6':
-                    this.checkIfItemISAvailable(magicSword, callback);
-                    this.showItemDescription(5);
-                    this.Load(callback);
-                    break;
-                case '7':
-                    this.checkIfItemISAvailable(brassKey, callback);
-                    this.showItemDescription(6);
-                    this.Load(callback);
-                    break;
-                case '8':
-                    this.checkIfItemISAvailable(rawGemStone, callback);
-                    this.showItemDescription(7);
-                    this.Load(callback);
-                    break;
-                case '9':
-                    this.checkIfItemISAvailable(fullFlask, callback);
-                    this.showItemDescription(8);
-                    this.Load(callback);
-                    break;
-                case 'b':
-                case 'B':
-                    Console.Clear();
-                    callback(null);
-                    break;
-                case 'q':
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.Clear();
-                    Console.WriteLine("Opção inválida!\n\n\n");
-                    this.Load(callback);
-                    break;
-            }
-        }*/
-
         private void checkIfItemISAvailable(bool item, CallbackStageMenu callback)
         {
             if (!item)
             {
                 Console.Clear();
                 Console.WriteLine("Opção inválida!\n\n\n");
-                this.Load(callback);
+                //this.Load(callback);
             }
         }
 
