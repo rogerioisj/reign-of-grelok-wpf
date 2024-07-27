94a3c80f372ad6643e312904795d02bdc02213af
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-27 16:58:28 -0300

    feat: create class to handle with menu itens

---
+++ InfoModel/MenuItem.cs
@@ -0,0 +1,32 @@
+﻿using reign_of_grelok_wpf.stages;
+
+namespace reign_of_grelok_wpf.infoModel
+{
+    class MenuItem
+    {
+        private readonly string Title;
+        private readonly MenuAction Action;
+
+        public MenuItem(string title, MenuAction action)
+        {
+            this.Title = title;
+            this.Action = action;
+        }
+
+        public string getTitle() { return this.Title; }
+
+        public MenuAction getAction() { return this.Action; }
+    }
+
+    class StageMenuItem : MenuItem
+    {
+        public bool isAvailable {  get; }
+
+        public StageMenuItem(string title, MenuAction action, bool isAvailable) : base(title, action)
+        {
+            this.isAvailable = isAvailable;
+        }
+
+        public MenuItem GetMenuItem() { return this; }
+    }
+}
