6c3e8259dbc5a61a22e21358b1a91cd4004e369b
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-28 02:19:56 -0300

    feat: create abstract class to handle with events to update menu list

---
+++ stages/IStage.cs
@@ -0,0 +1,9 @@
+﻿using reign_of_grelok_wpf.infoModel;
+
+namespace reign_of_grelok_wpf.stages
+{
+    interface IStage
+    {
+        public StageInfo LoadStageInfo(LoadStageAction backAction);
+    }
+}
