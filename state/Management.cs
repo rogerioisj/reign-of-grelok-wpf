ee763ec221cffebbc84df75200a8275f4e3da937
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-30 17:12:17 -0300

    feat: add summaries

--- state/Management.cs
+++ state/Management.cs
@@ -2,6 +2,9 @@
 
 namespace reign_of_grelok_wpf.state
 {
+    /// <summary>
+    /// Class to handle with state management
+    /// </summary>
     class Management
     {
         private bool hasSawTown;
@@ -28,48 +31,101 @@ namespace reign_of_grelok_wpf.state
             hasFinishedGame = false;
         }
 
+        /// <summary>
+        /// Method to change state when the player clicks on "See around" at town stage.
+        /// </summary>
         public void SeeTown() { hasSawTown = true; }
 
+        /// <summary>
+        /// Method to change state when the player clicks on "See around" at chapel stage.
+        /// </summary>
         public void SeeChapel() { hasSawChapel = true; }
 
+        /// <summary>
+        /// Method to change state when the player clicks on "See around" at swamp stage.
+        /// </summary>
         public void SeeSwamp() { hasSawSwamp = true; }
 
+        /// <summary>
+        /// Method to change state when the player clicks on "See around" at montain side stage.
+        /// </summary>
         public void SeeMontainside() { hasSawMontainside = true; }
 
+        /// <summary>
+        /// Method to check player already killed the zombie at chapel.
+        /// </summary>
         public void KillZombie() { hasKilledZombie = true; }
 
+        /// <summary>
+        /// Method to get the zombie head at chapel.
+        /// </summary>
         public void GetZombieHead() { hasTakedZombieHead = true; }
 
+        /// <summary>
+        /// Method to use the key at chapel.
+        /// </summary>
         public void UseKey() { hasUsedKey = true; }
 
+        /// <summary>
+        /// Method to get the gem at montainside.
+        /// </summary>
         public void GetRawGem() { hasTakedGem = true; }
 
+        /// <summary>
+        /// Method to talk to the wizard at swamp.
+        /// </summary>
         public void TalkToWizard() {  hasTalkedToWizard = true; }
 
+        /// <summary>
+        /// Method to store event to clear the raw gem.
+        /// </summary>
         public void ClearGem() {  hasClearGem = true; }
 
+        /// <summary>
+        /// Method to store event when the game is finished.
+        /// </summary>
         public void FinishGame() { hasFinishedGame = true; }
 
-        public bool AlreadyCheckTown() { return hasSawTown; }
-
-        public bool AlreadyCheckChapel() { return hasSawChapel; }
-
-        public bool AlreadyCheckSwamp() { return hasSawSwamp; }
-
-        public bool AlreadyCheckMontainside() { return hasSawMontainside; }
-
+        /// <summary>
+        /// Method to check if the player already kill the zombie.
+        /// </summary>
+        /// <returns>Returns a bool</returns>
         public bool AlreadyKilledZombie() { return hasKilledZombie; }
 
+        /// <summary>
+        /// Method to check if the player already used the key at chapel.
+        /// </summary>
+        /// <returns>Returns a bool</returns>
         public bool AlreadyUsedKey() { return hasUsedKey; }
 
+        /// <summary>
+        /// Method to check if the player already talked to the wizard.
+        /// </summary>
+        /// <returns>Returns a bool</returns>
         public bool AlreadyTalkedToWizard() { return hasTalkedToWizard; }
 
+        /// <summary>
+        /// Method to check if the player already take the gem at montain side.
+        /// </summary>
+        /// <returns>Returns a bool</returns>
         public bool AlreadyTakedGem() { return hasTakedGem; }
 
+        /// <summary>
+        /// Method to check if the player already get the zombie head at chapel.
+        /// </summary>
+        /// <returns>Returns a bool</returns>
         public bool AlreadyTakedZombieHead() { return hasTakedZombieHead; }
 
+        /// <summary>
+        /// Method to check if the player already clear the gem with the wizard.
+        /// </summary>
+        /// <returns>Returns a bool</returns>
         public bool AlreadyClearGem() { return hasClearGem; }
 
+        /// <summary>
+        /// Method to check if the player already finished the game.
+        /// </summary>
+        /// <returns>Returns a bool</returns>
         public bool AlreadyFinishedGame() { return hasFinishedGame; }
     }
 }
