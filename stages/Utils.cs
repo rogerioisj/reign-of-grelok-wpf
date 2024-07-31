5b230b7d6910bb33c01263d8dd03c70e22737d3f
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-31 12:32:33 -0300

    feat: add summaries

--- stages/Utils.cs
+++ stages/Utils.cs
@@ -2,7 +2,22 @@
 
 namespace reign_of_grelok_wpf.stages
 {
+    /// <summary>
+    /// Delegate to call a method to load a menu when the back button is pressed
+    /// </summary>
     delegate void CallbackStageMenu();
+
+    /// <summary>
+    /// Delegate to call a function that shows a button text
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>The text to be presented</returns>
     delegate string ShowTextAction(string? key);
+
+    /// <summary>
+    /// Delegate to call a method that returns a StageInfo
+    /// </summary>
+    /// <param name="action"></param>
+    /// <returns>The menu and stage description</returns>
     delegate StageInfo LoadStageAction(LoadStageAction? action);
 }
